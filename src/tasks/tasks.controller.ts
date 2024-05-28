ffee4d107ac66baaf70be337761df32e84d850e6
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-28 18:04:44 -0500

    feat(project): :sparkles: Crear y Listar

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { createTaskDto } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
 
+    constructor(private tasksService: TasksService) {
+    }
+
     @Get()
-    helloWorld() {
-        return 'Hello World!';
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() newTask: createTaskDto) {
+        return this.tasksService.createTask(newTask.title, newTask.description);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [{
+        id: '1',
+        title: 'Task 1',
+        description: 'Description 1',
+        status: TaskStatus.PENDING
+    }];
+
+    getAllTasks() {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string) {
+        const task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.PENDING,
+        };
+        this.tasks.push(task);
+        
+        return task;
+    }
+}
