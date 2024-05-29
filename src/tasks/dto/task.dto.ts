e574774b484070fa35b4c2537f700b85bbef6f12
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-29 08:41:44 -0500

    feat(project): :sparkles: Validaciones en NestJs

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,28 @@
 import { TaskStatus } from "../tasks.entity";
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class createTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
   title: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3) 
+
   description: string;
 }
 
 export class updateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status?: TaskStatus;
 }
